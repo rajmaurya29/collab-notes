70dfa8efb5157b1dd2200e869d08895d76988c34
Raj Maurya <rajmaurya29092004@gmail.com>
2026-02-12 22:21:04 +0530

    share_token added in model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 
@@ -13,3 +14,5 @@ class Note(models.Model):
     )
     created_at=models.DateField(auto_now_add=True)
     updated_at=models.DateField(auto_now=True)
+    share_token=models.UUIDField(default=uuid.uuid4,unique=True,null=True,blank=True,editable=False)
+    is_shared=models.BooleanField(default=False)
